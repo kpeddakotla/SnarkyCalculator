2a991fc0dabcc657a26243c0e54c19400c402627
Kushal Peddakotla <68529075+kpeddakotla@users.noreply.github.com>
2022-12-30 12:23:16 -0800

    V3

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
-import pytube
 import random
 import time
 import tkinter as tk
+from tkinter import font
+import sympy
 
 pygame.init()
 pygame.mixer.music.load("XXXTENTACION - Moonlight (Super Slow 0,75x).mp3")
-pygame.mixer.music.set_volume(0.25)
+pygame.mixer.music.set_volume(0.15)
 
 
 def calculate():
@@ -16,6 +17,12 @@ def calculate():
     # Check if the user wants to quit
     if equation == 'q':
         root.destroy()
+
+    if 'E' in equation:
+        user_input = ''.join(c for c in equation if c.isdigit() or c == '.')
+        user_input = float(equation)
+    equation = equation.replace('^', '**')
+    equation = equation.replace('pi', str(sympy.pi))
 
     # Reset the labels
     result_label['text'] = ""
@@ -86,44 +93,66 @@ def calculate():
     elif result < 0:
         comment_label['text'] = "Let's TRY to be more positive, okay?"
     elif result == 0:
-        comment_label['text'] = "Look its the amount of friends you have!"
+        comment_label['text'] = "Look! Its the amount of friends you have!"
     else:
-        comment_label['text'] += "\nLook at you go!"
-        comment_label['text'] += "\nProud of you cuz."
+        comments = ["You must be using a calculator or something...",
+                    "You're a wizard Harry. \nA math wizard!",
+                    "I'm proud of you, homes.",
+                    "Nice work, ese.",
+                    "Good stuff cuzzo.",
+                    "You couldn't live with your own failure, \nand where did that bring you? \n\nBack to me.",
+                    "Stream Back from You by Humdub on Spotify. \nNow.",
+                    "I would be jealous of your math skills. \nIf I wasn't a calculator.",
+                    "A system of cells. \nWithin cells interlinked. \nWithin one stem. \nAnd dreadfully distinct. \nAgainst the dark. \nA tall white fountain played.",
+                    "I would be jealous of your math skills. \nIf I wasn't a calculator.",
+                    "Just keep calculating. \nJust keep calculating. "]
+
+        comment = random.choice(comments)
+        comment_label['text'] = comment
 
 
 # Create the window
 root = tk.Tk()
 root['bg'] = "#222"
 root.title("Calculator")
-root.geometry("500x600")
+root.geometry("450x700")
+root.wm_title("The Snarky Calculator by Kushal P.")
+root.wm_iconbitmap("simple-calculator.ico")
 
 # Set the rows and columns to scale with the window size
 for i in range(6):
     root.grid_rowconfigure(i, weight=1)
     root.grid_columnconfigure(i, weight=1)
 
-# Create the title label
-title_label = tk.Label(root, text="The Snarky Calculator", font=("Helvetica", "30"), fg="#fff", bg='#222', wraplength=450)
-title_label.grid(row=0, column=0, columnspan=2, padx=20, pady=0, sticky="n")
-title_label = tk.Label(root, text="By Kushal P.", font=("Helvetica", "15"), fg="#fff", bg='#222', wraplength=450)
-title_label.grid(row=1, column=0, columnspan=2, padx=20, pady=0, sticky="n")
+# Create a frame to hold the widgets
+frame = tk.Frame(root, bg='#222')
+frame.grid(row=0, column=0)
+
+# Create the label with the main text and add it to the frame
+label = tk.Label(frame, text="The Snarky Calculator", font=("Helvetica", 30, "bold"), fg="#fff",bg='#222')
+label.pack()
+
+# Create a Text widget with the smaller text and add it to the frame
+text = tk.Text(frame, font=("Helvetica", 16, "normal"), height=1, width=20, state=tk.NORMAL, bd=0)
+text.insert(tk.END, "By Kushal P.")
+text.configure(fg="#fff", bg="#222")
+text.pack()
 
 # Create the input field with a dark foreground color
-input_field = tk.Entry(root, font=("Helvetica", 24), width=50, justify="center")
-input_field.grid(row=2, column=0, columnspan=2, padx=20, pady=20, sticky="n")
+input_field = tk.Entry(root, font=("Helvetica", 20), width=20, justify="center")
+input_field.grid(row=2, column=0, columnspan=2, padx=20, pady=40, sticky="nsew")
 
 # Create the calculate button with a smaller width
 calculate_button = tk.Button(root, text="Calculate", font=("Helvetica", 24), width=20, command=calculate, fg="#fff",
                              bg='#222')
-calculate_button.grid(row=3, column=0, columnspan=2, padx=20, pady=20, sticky="n")
+calculate_button.grid(row=3, column=0, columnspan=2, padx=20, pady=20, sticky="nsew")
 
 # Create the result label with a dark foreground color
-result_label = tk.Label(root, font=("Helvetica", 16), width=50, fg="#fff", bg='#222', wraplength=450)
+result_label = tk.Label(root, font=("Helvetica", 16), width=50, fg="#fff", bg='#222', wraplength=350)
 result_label.grid(row=4, column=0, columnspan=2, padx=20, pady=10, sticky="n")
 
 # Create the comment label with a dark foreground color
-comment_label = tk.Label(root, font=("Helvetica", 16), width=50, fg="#fff", bg='#222', wraplength=450)
+comment_label = tk.Label(root, font=("Helvetica", 14), width=50, fg="#fff", bg='#222', wraplength=350)
 comment_label.grid(row=5, column=0, columnspan=2, padx=20, pady=60, sticky="n")
 
 # Start the window loop
